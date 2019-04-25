46cb04b94d45ea0baab436f03c0be55d9c335c2d
Justin <justin.mugford@gmail.com>
2019-04-25 10:05:19 +1000

    Add level file for adding staff

--- CorsixTH/Lua/dialogs/mini_game.lua
+++ CorsixTH/Lua/dialogs/mini_game.lua
@@ -31,32 +31,32 @@ function UIMiniGame:UIMiniGame(ui)
 
   self.ui = ui
   self.world = app.world
-  
+
   -- these are just hacked into place here
-  
+
   self.original_adviser_state = self.ui.app.config.adviser_disabled
   -- disable without saving to config permanently
   self.ui.app.config.adviser_disabled = true
-  
+
   -- TODO: disable salary raise via another method as this will probably update config permanently
   self.original_debug_disable_salary_raise = self.world.debug_disable_salary_raise
   self.world.debug_disable_salary_raise = true
-  
+
   -- need to disable staff mood icons and the staff dialog.. they can't be clickable
   local hospital = self.world:getLocalPlayerHospital()
   for _, staff in ipairs(hospital.staff) do
   end
-  
+
   -- place our random litter everywhere... how many? one per tile?
   -- brute force get all tiles in plot
   local split_tiles = {}
   for intX = 1, 128 do
     for intY = 1, 128 do
-	  -- part of hospital and passable tiles to get mostly the internal ones
-	  if self.world:getHospital(intX, intY) and self.world.map.th:getCellFlags(intX, intY).buildable then
-	    table.insert(split_tiles, {[1]=intX, [2]=intY})
-	  end
-	end
+      -- part of hospital and passable tiles to get mostly the internal ones
+      if self.world:getHospital(intX, intY) and self.world.map.th:getCellFlags(intX, intY).buildable then
+        table.insert(split_tiles, {[1]=intX, [2]=intY})
+      end
+    end
   end
 
   local tile_count = #split_tiles
@@ -64,20 +64,18 @@ function UIMiniGame:UIMiniGame(ui)
   -- this places 100 litter, need to check how much
   for i = 1,100 do
      -- Drop some litter!
-	 local plot_tile_index = math.random(tile_count)
-	 local x, y =  split_tiles[plot_tile_index][1], split_tiles[plot_tile_index][2]
-	 -- table.remove
-	 split_tiles[plot_tile_index] = split_tiles[tile_count]
-	 split_tiles[tile_count] = nil
-	 tile_count = tile_count - 1
-	 
+     local plot_tile_index = math.random(tile_count)
+     local x, y =  split_tiles[plot_tile_index][1], split_tiles[plot_tile_index][2]
+     -- table.remove
+     split_tiles[plot_tile_index] = split_tiles[tile_count]
+     split_tiles[tile_count] = nil
+     tile_count = tile_count - 1
+
      local trash = math.random(1, 4)
      local litter = self.world:newObject("litter", x, y)
      litter:setLitterType(trash, math.random(0, 1))
   end
-  
 
-  
   self.on_top = false
   self.width = 640
   self.height = 48
@@ -88,7 +86,7 @@ function UIMiniGame:UIMiniGame(ui)
   self.white_font = app.gfx:loadFont("QData", "Font01V", 0, -2) -- need to find the black font for the floating kill numbers
 
   -- weapons will need to refer to sprite, position, sound, animation (for bomb)
-  self.weapons = {"shotgun", "uzi", "bazooka", "bomb"} 
+  self.weapons = {"shotgun", "uzi", "bazooka", "bomb"}
   self.current_weapon = "shotgun"
 
   self.rat_kills = 0
@@ -96,12 +94,12 @@ function UIMiniGame:UIMiniGame(ui)
   local litterness = self.world.map.th:getLitterFraction(1)
   print('litterness', litterness)
   self.litterness = litterness
-  
+
   -- brute force ratholes as we're going to need rats to spawn
   local wanted_ratholes = math.round(self.litterness * 200)
   if #hospital.ratholes < wanted_ratholes then
   end
-  
+
   self:addPanel( 10, 0, 0)
   self:addPanel( 11, 230, 0)
   self:addPanel( 12, 288, 0)
@@ -151,7 +149,7 @@ function UIMiniGame:restart()
   -- restoring the stuff we modified
   self.ui.app.config.adviser_disabled = self.original_adviser_state
   self.world.debug_disable_salary_raise = self.original_debug_disable_salary_raise
-  
+
   self.ui.app:restart()
 end
 
@@ -159,7 +157,7 @@ function UIMiniGame:quit()
   -- restoring the stuff we modified
   self.ui.app.config.adviser_disabled = self.original_adviser_state
   self.world.debug_disable_salary_raise = self.original_debug_disable_salary_raise
-  
+
   self.ui:quit()
 end
 
@@ -173,7 +171,7 @@ function UIMiniGame:draw(canvas, x, y)
     off = 19 * (5 - math.floor(math.log(self.rat_kills, 10)))
   end
   self.kills_font:draw(canvas, ("%i"):format(self.rat_kills), x + 72 + off, y + 14)
-  
+
   -- draw weapon
   if self.current_weapon == "shotgun" then
     self.panel_sprites:draw(canvas, 16, x + 230, y + 12)
@@ -183,8 +181,8 @@ function UIMiniGame:draw(canvas, x, y)
     self.panel_sprites:draw(canvas, 20, x + 329, y + 12)
   elseif self.current_weapon == "bomb" then
     self.panel_sprites:draw(canvas, 22, x + 387, y + 12)
-  end  
-  
+  end
+
   -- draw dirtiness meter
   -- litterness * 4
   local litterness_scale_factor = 32
@@ -200,29 +198,25 @@ function UIMiniGame:draw(canvas, x, y)
         self.panel_sprites:draw(canvas, 26, x + 431 + (24 * i), y + 12)
       end
     else
-	end
+    end
   else
     -- draw full_count garbage bins
-	for i= 1, full_count do
+    for i= 1, full_count do
       self.panel_sprites:draw(canvas, 26, x + 431 + (24 * i), y + 12)
-	end
-	litterness_scaled = litterness_scaled - full_count
-	if litterness_scaled > 0.67 then
-	  self.panel_sprites:draw(canvas, 25, x + 431 + (24 * full_count) + 24, y + 12)
-	elseif litterness_scaled > 0.33 then
-	  self.panel_sprites:draw(canvas, 24, x + 431 + (24 * full_count) + 24, y + 12)
-	end
+    end
+    litterness_scaled = litterness_scaled - full_count
+    if litterness_scaled > 0.67 then
+      self.panel_sprites:draw(canvas, 25, x + 431 + (24 * full_count) + 24, y + 12)
+    elseif litterness_scaled > 0.33 then
+      self.panel_sprites:draw(canvas, 24, x + 431 + (24 * full_count) + 24, y + 12)
+    end
   end
-  
-  
-
 end
 
 function UIMiniGame:setPosition(x, y)
   -- Lock to bottom of screen
   return Window.setPosition(self, x, -0.1)
 end
-
 
 function UIMiniGame:drawDynamicInfo(canvas, x, y)
 
@@ -239,8 +233,6 @@ function UIMiniGame:onMouseMove(x, y, dx, dy)
 --  end
   return true
 end
-
-
 
 function UIMiniGame:hitTest(x, y, x_offset)
   return x >= (x_offset and x_offset or 0) and y >= 0 and x < self.width and y < self.height
@@ -310,15 +302,15 @@ end
 --! Pop the message with the given index from the message queue and turn it into an actual
 -- message window; if no index is provided the first message in the queue is popped.
 function UIMiniGame:createMessageWindow(index)
+
 end
 
 function UIMiniGame:onTick()
   Window.onTick(self)
-  
+
   local litterness = self.world.map.th:getLitterFraction(1)
   self.litterness = litterness
 end
-
 
 function UIMiniGame:toggleInformation()
   self.world:toggleInformation()
